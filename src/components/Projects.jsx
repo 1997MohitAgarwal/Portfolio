4718a62c3bc15710515e5aa3f393cdcdd533f121
1997MohitAgarwal <152138497+1997MohitAgarwal@users.noreply.github.com>
2025-02-08 18:30:53 +0530

    fixes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,18 +32,18 @@ export default function Projects() {
       description: "Modify text styles, cases, and more.",
       link: "https://text-utility-pi.vercel.app/",
     },
-    {
-      img: "/assets/images/Chatbot.gif",
-      title: "AI Chatbot",
-      description: "A smart chatbot for answering queries.",
-      link: "https://chat-ai-nu-flame.vercel.app/",
-    },
-    {
-      img: "/assets/images/news.gif",
-      title: "News App",
-      description: "Fetches news based on different categories.",
-      link: "https://india-fission-news.vercel.app/",
-    },
+    // {
+    //   img: "/assets/images/Chatbot.gif",
+    //   title: "AI Chatbot",
+    //   description: "A smart chatbot for answering queries.",
+    //   link: "https://chat-ai-nu-flame.vercel.app/",
+    // },
+    // {
+    //   img: "/assets/images/news.gif",
+    //   title: "News App",
+    //   description: "Fetches news based on different categories.",
+    //   link: "https://india-fission-news.vercel.app/",
+    // },
     {
       img: "/assets/images/dashboard.gif",
       title: "Dashboard UI",
